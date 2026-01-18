09f2a7d4f9e94c7fa627e2b201d5c4c66f949aa9
Tomáš Kaňuch <tomas.kanuch@gmail.com>
2026-01-18 21:39:05 +0100

    Stage 4: SELECT, FROM, JOIN, WHERE, ORDER BY

---
+++ Query_Stage4.sql
@@ -0,0 +1,46 @@
+/*
+START STAGE 4
+*/
+student_inf = "SELECT full_name
+FROM Student s
+JOIN Operation o ON s.id = o.student_id
+WHERE o.book_id = 2
+ORDER BY s.full_name;"
+
+/*
+END STAGE 4
+*/
+
+/*
+START STAGE 1
+*/
+
+/*
+Other Solution
+--
+
+ */
+
+/*
+Other Solution
+--
+
+ */
+
+/*
+Other Solution
+--
+
+ */
+
+/*
+Other Solution
+--
+
+ */
+
+/*
+Other Solution
+--
+
+ */
